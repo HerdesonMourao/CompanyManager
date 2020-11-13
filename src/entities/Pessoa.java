87f6ecc4012ded1fc3082f782716962d604a0b1d
Herdeson Mourao <herdesondev@gmail.com>
2020-11-12 23:43:49 -0300

    Created Pessoa class

---
+++ src/entities/Pessoa.java
@@ -0,0 +1,48 @@
+package entities;
+
+public abstract class Pessoa {
+	protected String nome;
+	protected Telefone telefone;
+	protected Endereco endereco;
+	
+	//Constructors
+	public Pessoa(String nome, Telefone telefone, Endereco endereco) {
+		this.nome = nome;
+		this.telefone = telefone;
+		this.endereco = endereco;
+	}
+
+	//Gets and Sets
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Telefone getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(Telefone telefone) {
+		this.telefone = telefone;
+	}
+
+	public Endereco getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(Endereco endereco) {
+		this.endereco = endereco;
+	}
+	
+	//Methods
+	public abstract void realizarCadastro();
+	
+	public abstract void visualizarCadastro();
+	
+	public abstract void atualizarCadastro();
+	
+	public abstract void deletarCadastro();
+}
